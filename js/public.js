f9c31415db95a0a45977a1dd05872a218d8c9c71
Ondřej Doněk <ondrejd@gmail.com>
2018-03-20 18:35:48 +0100

    Added possibility to hide sections. Some minor fixies and improvements.

--- js/public.js
+++ js/public.js
@@ -3,10 +3,87 @@ jQuery( document ).ready( function() {
   // Helper variables
   var is_preview = false;
   var is_photo_uploaded = false;
+  var shown_dialog = null;
+  var form = jQuery( ".notices-generator form" );
+  var form_notice = jQuery( ".notices-generator .form-notice" );
+  var photo_notice  = jQuery( "#photo-upload-notice" );
+  var photo_preview = jQuery( "#photo-upload-preview" );
+  var photo_noimg = jQuery( "#photo-upload-noimg" );
+  var photo_file = form.find( "[name=photo-upload]" );
+  var photo_id = form.find( "[name=photo_id]" );
 
   // Functions for showing/hiding dialog
-  var showDlg = function( id ) { jQuery( id ).show(); window.scrollTo( 0, 0 ); };
-  var hideDlg = function( id ) { jQuery( id ).hide(); };
+  var showDlg = function( id ) {
+    jQuery( id ).show();
+    window.scrollTo( 0, 0 );
+  };
+
+  var hideDlg = function( id ) {
+    jQuery( id ).hide();
+  };
+
+  // Functions for turning preview mode on/off
+  var previewOff = function() {
+    jQuery( ".notices-generator form fieldset legend").show();
+    jQuery( ".notices-generator form fieldset .inline-editor" ).css( "border-color", "#000" );
+    jQuery( ".notices-generator form .description " ).show();
+    jQuery( ".notices-generator #ngform_sec4" ).show();
+    jQuery( ".notices-generator form fieldset .notice-survivors--form" ).show();
+    jQuery( "#odwpng-button_print" ).removeClass( "button-disabled" );
+    jQuery( "#odwpng-button_borders" ).removeClass( "button-disabled" );
+    jQuery( "#odwpng-button_background" ).removeClass( "button-disabled" );
+  };
+
+  var previewOn = function() {
+    // Schovat všechny názvy sekcí formuláře
+    jQuery( ".notices-generator form fieldset legend").hide();
+    /// schovat okraje editorů
+    jQuery( ".notices-generator form fieldset .inline-editor" ).css( "border-color", "transparent" );
+    // skryt popisky
+    jQuery( ".notices-generator form .description " ).hide();
+    // skrýt pole fotografie, pokud žádná není nahrána
+    jQuery( "#ngform_sec4" ).hide();
+    // skryjeme radioboxy u pozustalych
+    jQuery( ".notices-generator form fieldset .notice-survivors--form" ).hide();
+    // zakazeme tlacitka pro vyber okraju a pozadi
+    jQuery( "#odwpng-button_print" ).addClass( "button-disabled" );
+    jQuery( "#odwpng-button_borders" ).addClass( "button-disabled" );
+    jQuery( "#odwpng-button_background" ).addClass( "button-disabled" );
+
+    // pozustali (zjistime pocet sloupecku a vytvorime pole pozustalych, ktere vytiskneme)
+    var cols_count = parseInt( jQuery( "input[name=ng-survivors-columns]:checked", ".notices-generator form" ).val() );
+    var survivors_raw = jQuery( "#notice-survivors-editor" ).html().split( "," );
+    var survivors = new Array();
+
+    jQuery.each(
+      jQuery( "#notice-survivors-editor" ).html().split( "," ),
+      function( idx, val ){ survivors.push( ( new String( val ) ).trim() ); }
+    );
+
+    console.log( survivors );
+    // XXX Finish this!!!
+  };
+  
+  // XHR listener for photo upload
+  var photo_upload_xhr_listener = function( e ) {
+    console.log( "photo_upload_xhr_listener", e );
+    if( e.lengthComputable ) {
+      var perc = ( e.loaded / e.total ) * 100;
+      perc = perc.toFixed( 2 );
+      photo_notice.html( odwpng.msg02.replace( '%%DONE%%', perc ) );
+    }
+  };
+
+  // Watch if ESC was pressed...
+  jQuery( document ).keyup( function( e ) {
+    if( e.keyCode == 27 ) {
+      if( is_preview === true ) {
+        previewOff();
+      } else if( shown_dialog !== null ) {
+        hideDlg( shown_dialog );
+      }
+    }
+  } );
 
   /**
    * Images dialog
@@ -16,17 +93,20 @@ jQuery( document ).ready( function() {
     if( is_preview === true ) {
       return;
     }
-    showDlg( "#notice-images-dlg" );
+    shown_dialog = "#notice-images-dlg";
+    showDlg( shown_dialog );
   } );
   // 2) Close dialog
   jQuery( "#notice-images-dlg .button-cancel" ).click( function( e ) {
     hideDlg( "#notice-images-dlg" );
+    shown_dialog = null;
   } );
   // 3) Pick image and close dialog
   jQuery( "#notice-images-dlg .images-item--cont" ).click( function( e ) {
     var image = jQuery( this ).find( "img" ).attr( "src" );
     jQuery( "#selected_notice_img" ).attr( "src", image );
     hideDlg( "#notice-images-dlg" );
+    shown_dialog = null;
   } );
 
   /**
@@ -37,48 +117,34 @@ jQuery( document ).ready( function() {
     if( is_preview === true ) {
       return;
     }
-    showDlg( "#notice-verses-dlg" );
+    shown_dialog = "#notice-verses-dlg";
+    showDlg( shown_dialog );
   } );
   // 2) Close dialog
   jQuery( "#notice-verses-dlg .button-cancel" ).click( function( e ) {
     hideDlg( "#notice-verses-dlg" );
+    shown_dialog = null;
   } );
   // 3) Pick verse and close dialog
   jQuery( "#notice-verses-dlg .verses-list li" ).click( function( e ) {
     var verse = jQuery( this ).html();
     jQuery( "#notice-verse-editor" ).html( verse );
     hideDlg( "#notice-verses-dlg" );
+    shown_dialog = null;
   } );
 
   /**
    * Photo upload
    */
-  var form = jQuery( ".notices-generator form" );
-  var form_notice = jQuery( ".notices-generator .form-notice" );
-  var photo_notice  = jQuery( "#photo-upload-notice" );
-  var photo_preview = jQuery( "#photo-upload-preview" );
-  var photo_noimg = jQuery( "#photo-upload-noimg" );
-  var photo_file = form.find( "[name=photo-upload]" );
-  var photo_id = form.find( "[name=photo_id]" );
 
-  photo_file.on( "change", function( change_event ) {
-    change_event.preventDefault();
+  photo_file.on( "change", function( e ) {
+    e.preventDefault();
 
     var fdata = new FormData();
     fdata.append( "action", "upload-photo" );
     fdata.append( "photo-upload", photo_file[0].files[0] );
     fdata.append( "name", photo_file[0].files[0].name );
     fdata.append( "_wpnonce", odwpng.nonce );
-
-    var photo_upload_xhr_listener = function( xhr_event ) {
-console.log( xhr_event );
-      if( xhr_event.lengthComputable ) {
-        var perc = ( xhr_event.loaded / xhr_event.total ) * 100;
-        perc = perc.toFixed( 2 );
-
-        photo_notice.html( odwpng.msg02.replace( '%%DONE%%', perc ) );
-      }
-    };
 
     jQuery.ajax({
       url: odwpng.upload_url,
@@ -127,45 +193,49 @@ console.log( odwpng.msg04 );
           photo_id.val( "" );
         }
       }*/
-    });
+    } );
   } );
 
-  form.on( "click", ".btn-change-image", function( mouse_event ) {
-    mouse_event.preventDefault();
+  form.on( "click", ".btn-change-image", function() {
     photo_notice.empty().hide();
     photo_file.val( "" ).show();
     photo_id.val( "" );
     photo_preview.empty().hide();
-  });
+  } );
 
   photo_file.on( "click", function() {
     photo_file.val( "" );
     photo_id.val( "" );
-  });
+  } );
 
   /**
    * Borders dialog
    */
   // 1) Open dialog
-  jQuery( "#odwpng-button_borders" ).click( function( e ) {
+  jQuery( "#odwpng-button_borders" ).click( function() {
     if( is_preview === true ) {
       return;
     }
     showDlg( "#notice-borders-dlg" );
+    shown_dialog = "#notice-borders-dlg";
   } );
   // 2) Close dialog
-  jQuery( "#notice-borders-dlg .button-cancel" ).click( function( e ) {
+  jQuery( "#notice-borders-dlg .button-cancel" ).click( function() {
     hideDlg( "#notice-borders-dlg" );
+    shown_dialog = null;
   } );
   // 3) Pick border and close dialog
-  jQuery( "#notice-borders-dlg .borders-item" ).click( function( e ) {
+  jQuery( "#notice-borders-dlg .borders-item" ).click( function() {
     var border = jQuery( this ).text();
+
     for( var i = 1; i < 5; i++ ) {
       jQuery( ".notices-generator--cont" ).removeClass( "border-0" + i );
     }
+
     jQuery( ".notices-generator--cont" ).addClass( "border-" + border );
     jQuery( "#odwpng-notice_border" ).val( "border" + border );
     hideDlg( "#notice-borders-dlg" );
+    shown_dialog = null;
   } );
 
   /**
@@ -181,47 +251,20 @@ console.log( odwpng.msg04 );
   /**
    * Preview button
    */
-  /* XXX Finish this */
-  jQuery( "#odwpng-button_preview" ).click(function() {
+  jQuery( "#odwpng-button_preview" ).click( function() {
     if( is_preview !== true ) {
-      /* 1) schovat všechny názvy sekcí formuláře */
-      jQuery( ".notices-generator form fieldset legend").hide();
-      /* 2) schovat okraje editorů */
-      jQuery( ".notices-generator form fieldset .inline-editor" ).css( "border-color", "transparent" );
-      /* 3) skryt popisky */
-      jQuery( ".notices-generator form .description " ).hide();
-      /* 4) skrýt pole fotografie, pokud žádná není nahrána */
-      jQuery( "#ngform_sec4" ).hide();
-      /* 5) skryjeme radioboxy u pozustalych */
-      jQuery( ".notices-generator form fieldset .notice-survivors--form" ).hide();
-      /* 6) zakazeme tlacitka pro vyber okraju a pozadi */
-      jQuery( "#odwpng-button_print" ).addClass( "button-disabled" );
-      jQuery( "#odwpng-button_borders" ).addClass( "button-disabled" );
-      jQuery( "#odwpng-button_background" ).addClass( "button-disabled" );
-      /* 7) musime preformatovat pozustale */
-      // zjistime pocet sloupecku
-      var cols_count = parseInt( jQuery( "input[name=ng-survivors-columns]:checked", ".notices-generator form" ).val() );
-      console.log( cols_count );
-      // vytvorime pole pozustalych
-      var survivors_raw = jQuery( "#notice-survivors-editor" ).html().split( "," );
-      var survivors = new Array();
-      jQuery.each(
-        jQuery( "#notice-survivors-editor" ).html().split( "," ),
-        function( idx, val ){ survivors.push( ( new String( val ) ).trim() ); }
-      );
-      console.log( survivors );
+      previewOn();
     } else {
-      jQuery( ".notices-generator form fieldset legend").show();
-      jQuery( ".notices-generator form fieldset .inline-editor" ).css( "border-color", "#000" );
-      jQuery( ".notices-generator form .description " ).show();
-      jQuery( ".notices-generator #ngform_sec4" ).show();
-      jQuery( ".notices-generator form fieldset .notice-survivors--form" ).show();
-      jQuery( "#odwpng-button_print" ).removeClass( "button-disabled" );
-      jQuery( "#odwpng-button_borders" ).removeClass( "button-disabled" );
-      jQuery( "#odwpng-button_background" ).removeClass( "button-disabled" );
+      previewOff();
     }
 
     is_preview = !is_preview;
   } );
 
+  /**
+   * Hide section links
+   */
+  jQuery( ".disable-part-link" ).click( function( mouse_event ) {
+    jQuery( this ).parent().siblings().toggle();
+  } );
 } );
